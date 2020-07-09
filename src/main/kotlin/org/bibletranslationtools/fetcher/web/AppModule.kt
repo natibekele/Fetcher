430df3242f61120d74e1bd6e55a26a41926bc8f6
DJ <djpric91@gmail.com>
2020-07-09 16:26:57 -0400

    some initial card views of languages

--- src/main/kotlin/org/bibletranslationtools/fetcher/web/AppModule.kt
+++ src/main/kotlin/org/bibletranslationtools/fetcher/web/AppModule.kt
@@ -1,11 +1,19 @@
 package org.bibletranslationtools.fetcher.web
 
 import dev.jbs.ktor.thymeleaf.Thymeleaf
+import dev.jbs.ktor.thymeleaf.ThymeleafContent
 import io.ktor.application.Application
+import io.ktor.application.call
 import io.ktor.application.install
 import io.ktor.features.CallLogging
 import io.ktor.features.DefaultHeaders
+import io.ktor.http.content.resources
+import io.ktor.http.content.static
+import io.ktor.response.respond
 import io.ktor.routing.Routing
+import io.ktor.routing.get
+import io.ktor.routing.routing
+import org.bibletranslationtools.fetcher.data.Language
 import org.thymeleaf.templateresolver.ClassLoaderTemplateResolver
 
 fun Application.appModule() {
@@ -19,5 +27,25 @@ fun Application.appModule() {
     }
     install(CallLogging)
     install(Routing) {
+        routing {
+            static("static") {
+                resources("css")
+            }
+            get("/languages") {
+                call.respond(
+                    ThymeleafContent(
+                        template = "languages",
+                        model = mapOf(
+                            "languageList" to listOf(
+                                Language("en", "English", "English"),
+                                Language("fr", "French", "France"),
+                                Language("en", "English", "English"),
+                                Language("fr", "French", "France")
+                            )
+                        )
+                    )
+                )
+            }
+        }
     }
 }
