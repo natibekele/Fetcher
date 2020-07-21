189f7d15a52cfd5e22b126b31e2c26cdad82475d
DJ <djpric91@gmail.com>
2020-07-21 10:14:00 -0400

    boilerplate for navigation

--- src/main/kotlin/org/bibletranslationtools/fetcher/web/AppModule.kt
+++ src/main/kotlin/org/bibletranslationtools/fetcher/web/AppModule.kt
@@ -1,12 +1,18 @@
 package org.bibletranslationtools.fetcher.web
 
 import dev.jbs.ktor.thymeleaf.Thymeleaf
+import dev.jbs.ktor.thymeleaf.ThymeleafContent
 import io.ktor.application.Application
 import io.ktor.application.ApplicationCallPipeline
+import io.ktor.application.call
 import io.ktor.application.install
 import io.ktor.features.CallLogging
 import io.ktor.features.DefaultHeaders
+import io.ktor.response.respond
+import io.ktor.response.respondText
 import io.ktor.routing.Routing
+import io.ktor.routing.get
+import io.ktor.routing.route
 import org.thymeleaf.templateresolver.ClassLoaderTemplateResolver
 
 fun Application.appModule() {
@@ -23,5 +29,60 @@ fun Application.appModule() {
         // display error page here and terminate the pipeline if fatal exception occurs
     }
     install(Routing) {
+        route("/") {
+            get {
+                call.respond(
+                    ThymeleafContent(
+                        template = "index",
+                        model = mapOf()
+                    )
+                )
+            }
+            route("gl") {
+                get {
+                    call.respond(
+                        ThymeleafContent(
+                            template = "languages",
+                            model = mapOf()
+                        )
+                    )
+                }
+                route("{languageCode}") {
+                    get {
+                        call.respond(
+                            ThymeleafContent(
+                                template = "products",
+                                model = mapOf()
+                            )
+                        )
+                    }
+                    route("{productSlug}") {
+                        get {
+                            call.respond(
+                                ThymeleafContent(
+                                    template = "books",
+                                    model = mapOf(
+                                        "languageCode" to call.parameters["languageCode"]!!
+                                    )
+                                )
+                            )
+                        }
+                        route("{bookSlug}") {
+                            get {
+                                call.respond(
+                                    ThymeleafContent(
+                                        template = "chapters",
+                                        model = mapOf(
+                                            "languageCode" to call.parameters["languageCode"]!!,
+                                            "productSlug" to call.parameters["productSlug"]!!
+                                        )
+                                    )
+                                )
+                            }
+                        }
+                    }
+                }
+            }
+        }
     }
 }
